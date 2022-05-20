7ea947fc99c9f478dc2b907e7cb80e777237c094
James Kyan <james.kyan88@gmail.com>
2022-05-20 12:18:30 -0400

    commit tree 79f9f7b2863cfac70baa7447875b69f62c9a178b

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-// Code your solutions in this file
+// const gifts = ['teddy bear', 'drone', 'doll'];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+const messages = []
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+    let msg = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+    messages.push(msg);
+}
+    return messages;
+}
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   let i = 0; // the initialization moves OUTSIDE the body of the loop!
+//   while (i < gifts.length) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     i++; // the iteration moves INSIDE the body of the loop!
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+// LOG: Wrapped teddy bear and added a bow!
+// LOG: Wrapped drone and added a bow!
+// LOG: Wrapped doll and added a bow!
+// => ["teddy bear", "drone", "doll"]
+
+// function plantGarden() {
+//     let keepWorking = true;
+//     while (keepWorking) {
+//       chooseSeedLocation();
+//       plantSeed();
+//       waterSeed();
+//       keepWorking = checkForMoreSeeds();
+//     }
+//   }
+
+
+function countDown() {
+    let n = 10
+    while (n > -1) {
+        console.log(n);
+        n--
+    }
+}
+
+// while needs iteration inside loop and initialization is outside always
